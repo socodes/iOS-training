3db29816ee947088aa24fe7052083155b9d17534
Muhammed Didin <mdidin80@gmail.com>
2021-10-09 14:24:31 +0300

    SimpsonBook

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,21 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Muhammed Didin on 9.10.2021.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image = UIImage()
+    
+    init(simpsonName:String,simpsonJob:String,simpsonImage:UIImage){
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+}
